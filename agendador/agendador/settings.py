fb3477cb5f05b51571e8175dbcca7dfe85b0af6e
Rodrigo Reiniger <reiniger.95@gmail.com>
2025-02-08 16:15:07 -0300

    att-settings

--- agendador/agendador/settings.py
+++ agendador/agendador/settings.py
@@ -10,10 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+import environ
 from pathlib import Path
+# Inicialize o django-environ
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
+# Leia o arquivo .env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
